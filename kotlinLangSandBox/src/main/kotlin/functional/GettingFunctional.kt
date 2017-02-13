65b5802a1f5cd93782ad33f40a8b9356fc7d1760
Victor <em3888@gmail.com>
2017-02-14 02:56:06 +0800

    Add some more notes on Hadi
    he just piss off everyone under the sun, Good MAN

--- kotlinLangSandBox/src/main/kotlin/functional/GettingFunctional.kt
+++ kotlinLangSandBox/src/main/kotlin/functional/GettingFunctional.kt
@@ -3,10 +3,22 @@ package functional
 /**
  * Created by victor888 on 2/2/2017.
  */
+
 fun operation(x: Int, y: Int, op: (Int, Int) -> Int): Int {
     return op(x, y)
 }
 
+//Todo check out this website http://hadihariri.com/2013/11/09/refactoring-to-functional-basic-primitives/
+//For getting functional, this guy piss off lot of people
+fun main(args: Array<String>) {
+
+    var somethng = "String with .Var"//this works really well
+
+    println(operation(12, 25,::Multiplyit)) //passing into function work really well
+
+    val sumLamda :(Int,Int)-> Int ={x,y -> x+y}
+
+}
 
 fun route(path: String, vararg action: (String, String) -> String) {
 
@@ -19,13 +31,3 @@ fun UnaryOperation(x: Int, op: (Int) -> Int) {
 fun Sumit(x: Int, y: Int) = x + y
 
 fun Multiplyit(x: Int,y:Int) = x*y
-
-fun main(args: Array<String>) {
-
-    var somethng = "String with .Var"//this works really well
-
-    println(operation(12, 25,::Multiplyit)) //passing into function work really well
-
-    val sumLamda :(Int,Int)-> Int ={x,y -> x+y}
-
-}
--- kotlinLangSandBox/src/main/kotlin/functions/Using.kt
+++ kotlinLangSandBox/src/main/kotlin/functions/Using.kt
@@ -5,7 +5,9 @@ import java.io.Closeable
 /**
  * Created by victor888 on 2/13/2017.
  */
-
+//C# had using keyword but kotlin dont, here is what we get
+//Someone said use "use"
+//todo figure out how to use keyword
 fun using(obj: Closeable, block: () -> Unit) {
   try {
     block()
