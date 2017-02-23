f5b15b8b52bdb10fea4a607675944ac825a550d9
Victor <em3888@gmail.com>
2017-02-23 18:15:29 +0800

    Move enum to class
    because enum is a class not a struct

--- kotlinLangSandBox/src/main/kotlin/class/Enums.kt
+++ kotlinLangSandBox/src/main/kotlin/class/Enums.kt
@@ -9,30 +9,36 @@ package `class`
 //Enum is class, can take constructor
 fun main(args: Array<String>) {
 var index =0
+  printEnumInnerValue(index)
 
 
-  for (e: Colors in Colors.values())
-  {
+
+}
+
+fun printEnumInnerValue(index: Int) {
+  var index1 = index
+  for (e: Colors in Colors.values()) {
     println("${e.name}(${e.item.joinToString(",")})")
   }
 
   for (c: Colors in Colors.values()) {
 
-    println("${c.name} + ${c.item[index]}")
+    println("${c.name} + ${c.item[index1]}")
     //RED (255,0,0)
     //GREEN(0,128,0)
     //BLUE(0,0,255)
-    index++
+    index1++
     //Todo -> Need to figure out how to access the inner value of Enum Class
   }
 }
 
 enum class Colors(val item:IntArray){
+  //Array is mutable, Enum can be break into pieces
    RED(intArrayOf(255,0,0)),
    GREEN(intArrayOf(0,128,0)),
    BLUE(intArrayOf(0,0,255))
 }
-//Array is mutable, Enum can be break into pieces
+
 
 
 
